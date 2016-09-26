caee32533bd3f96c7650a85024b3a7a182141c49
Megan Talbot <meganftalbot@gmail.com>
2016-09-26 15:41:16 -0600

    Extension 1: added colors based on guess. Needs to be refactored

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,13 +6,28 @@ SECRET_NUMBER = rand(100)
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  background = background_color(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :background => background}
 end
 
 def check_guess(guess)
-  return "Take a guess!" if guess == nil
-  return "Too high!" if guess > SECRET_NUMBER
-  return "WAY too high!" if guess > SECRET_NUMBER + 5
-  return "WAY too low!" if guess < SECRET_NUMBER - 5
-  return "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}." if guess == SECRET_NUMBER
+  difference = (SECRET_NUMBER - guess).abs
+  if guess < SECRET_NUMBER
+    difference > 5 ? "Way too low!" : "Too low!"
+  elsif guess > SECRET_NUMBER
+    difference > 5 ? "Way too high!" : "Too high!"
+  else
+    "You got it right! <br> The Secret Number was #{SECRET_NUMBER}!"
+  end
+end
+
+def color(message)
+  case message
+    when "Way too low!" || "Way too high!"
+      'red'
+    when "Too low!" || "Too high!"
+      '#FCDFDC'
+    when "You got it right!"
+      'green'
+  end
 end
